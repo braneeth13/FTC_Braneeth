e78288dec9e0a8a031f070d5982ce70809944024
Pratham Juvekar <55061986+23pjuvekar@users.noreply.github.com>
2020-02-21 11:03:52 -0500

    Created basic controls for robot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopModeDrive.java
@@ -0,0 +1,142 @@
+package org.firstinspires.ftc.robotcontroller.external.samples;
+
+// Importing the needed packages for code
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+
+@TeleOp(name="Basic: Iterative OpMode", group="Iterative Opmode")
+@Disabled
+public class BasicOpMode_Iterative extends OpMode
+{
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftFrontMotor = null;
+    private DcMotor rightFrontMotor = null;
+    private DcMotor leftBackMotor = null;
+    private DcMotor rightBackMotor = null;
+
+    // Sets variables to 0, to reset them before they are used
+    double leftTrigger = 0;
+    double rightTrigger = 0;
+    double left_stick_X = 0;
+    double left_stick_Y = 0;
+    double right_stick_X = 0;
+    double right_stick_Y = 0;
+
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void init() {
+        telemetry.addData("Status", "Initialized");
+
+        // Initialize the hardware variables
+
+        leftFrontMotor  = hardwareMap.get(DcMotor.class, "lfm");
+        rightFrontMotor = hardwareMap.get(DcMotor.class, "rfm");
+        leftBackMotor  = hardwareMap.get(DcMotor.class, "lbm");
+        rightBackMotor = hardwareMap.get(DcMotor.class, "rbm");
+
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the battery
+
+        leftFrontMotor.setDirection(DcMotor.Direction.FORWARD);
+        rightFrontMotor.setDirection(DcMotor.Direction.REVERSE);
+        leftBackMotor.setDirection(DcMotor.Direction.FORWARD);
+        rightBackMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        // Tell the driver that initialization is complete.
+        telemetry.addData("Status", "Initialized");
+
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+        runtime.reset();
+    }
+
+    void moveRegular(double power) {
+        leftFrontMotor.setDirection(DcMotor.Direction.FORWARD);
+        rightFrontMotor.setDirection(DcMotor.Direction.REVERSE);
+        leftBackMotor.setDirection(DcMotor.Direction.FORWARD);
+        rightBackMotor.setDirection(DcMotor.Direction.REVERSE);
+        leftFrontMotor.setPower(power);
+        leftBackMotor.setPower(power);
+        rightFrontMotor.setPower(power);
+        rightBackMotor.setPower(power);
+    }
+
+    void moveLeft(double power) {
+        leftFrontMotor.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontMotor.setDirection(DcMotor.Direction.REVERSE);
+        leftBackMotor.setDirection(DcMotor.Direction.REVERSE);
+        rightBackMotor.setDirection(DcMotor.Direction.REVERSE);
+        leftFrontMotor.setPower(power);
+        leftBackMotor.setPower(power);
+        rightFrontMotor.setPower(power);
+        rightBackMotor.setPower(power);
+    }
+
+    void moveRight(double power) {
+        leftFrontMotor.setDirection(DcMotor.Direction.FORWARD);
+        rightFrontMotor.setDirection(DcMotor.Direction.FORWARD);
+        leftBackMotor.setDirection(DcMotor.Direction.FORWARD);
+        rightBackMotor.setDirection(DcMotor.Direction.FORWARD);
+        leftFrontMotor.setPower(power);
+        leftBackMotor.setPower(power);
+        rightFrontMotor.setPower(power);
+        rightBackMotor.setPower(power);
+    }
+        /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+
+        // Assigns the values to the variables that are needed
+
+        rightTrigger = gamepad1.right_trigger;
+        leftTrigger = -gamepad1.left_trigger;
+        left_stick_X = gamepad1.left_stick_X;
+        left_stick_Y = gamepad1.left_stick_Y;
+        right_stick_X = gamepad1.left_stick_X;
+        right_stick_Y = gamepad1.leftstick_Y;
+
+        // allows robot to move fowards and backwards
+        if (left_stick_X = 0) {
+            moveRegular(rightTrigger+leftTrigger);
+        } else if (left_stick_X < 0) {
+            moveLeft(rightTrigger+leftTrigger);
+        } else if (left_stick_X > 0) {
+            moveRight(rightTrigger+leftTrigger);
+        }
+
+        // Show the elapsed game time and wheel power.
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+    }
+
+}
